b23509f240f427c80dc3a043f0e65ba18a1be4c4
Jayesh Mahajan <jayeshmahajan119@gmail.com>
2024-03-07 20:27:17 +0530

    Delete EBookStore directory

--- EBookStore/src/main/java/com/bookStore/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookStore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookStore.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer>{
-
-}
